fdc5d869fcde2740ec9361c0d26134ca7ee24fe5
arkadiusz <szastarek@gmail.com>
2019-05-15 19:45:06 +0200

    static login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './core/app.routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { CustomMaterialModule } from './core/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CustomMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/app.routing.module.ts
@@ -0,0 +1,25 @@
+
+import { UserComponent } from '../user/user.component';
+
+import { LoginComponent } from '../login/login.component';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    { path: 'user', component: UserComponent },
+    { path: 'login', component: LoginComponent },
+    {path : '', component : LoginComponent}
+  ];
+  
+  @NgModule({
+    imports: [
+      RouterModule.forRoot(routes),
+    ],
+    exports: [
+      RouterModule
+    ],
+    declarations: []
+  })
+  export class AppRoutingModule { }
+  
